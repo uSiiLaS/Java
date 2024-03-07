2b977885c0e9c294a8a42ca9cb6f64853bf51b7b
uSiiLaS <142810322+uSiiLaS@users.noreply.github.com>
2024-03-07 11:11:13 -0300

    Add files via upload

---
+++ Java 3.java
@@ -0,0 +1,10 @@
+public class Main{
+    
+    public static void main(String[]args){
+        
+        String[] produto = {"manga", "maracuja", "laranja"};
+        
+        System.out.printf(produto[2]);
+        
+    }
+}
